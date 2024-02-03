eecc6a4761df41f0332b1fd851edbeaf0423eab7
alif <malifalhakim11@gmail.com>
2024-02-03 20:32:01 +0700

    Add edit and findById methods to ProductService Interface

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -6,4 +6,6 @@ import java.util.List;
 public interface ProductService {
     public Product create(Product product);
     public List<Product> findAll();
+    public Product edit(Product product,String id);
+    public Product findById(String id);
 }
