f339fa7c3bb5f89e16df678ff42aa0b36ffdf745
alif <malifalhakim11@gmail.com>
2024-02-03 20:30:37 +0700

    Add edit method (to edit product properties) and findById method (to find product based on Id) to ProductRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -10,13 +10,37 @@ import java.util.List;
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
+    private static int idCounter = 0;
 
     public Product create(Product product){
+        product.setProductId("P" + idCounter++);
         productData.add(product);
         return product;
     }
 
     public Iterator<Product> findAll(){
         return productData.iterator();
+    }
+
+    public Product edit(Product product, String id){
+        product.setProductId(id);
+        Product oldProduct = findById(id);
+        int index = productData.indexOf(oldProduct);
+
+        // if product found
+        if (index >= 0){
+            productData.set(index, product);
+            return product;
+        }
+        return null;
+    }
+
+    public Product findById(String id){
+        for (Product product: productData){
+            if (product.getProductId().equals(id)){
+                return product;
+            }
+        }
+        return null;
     }
 }
