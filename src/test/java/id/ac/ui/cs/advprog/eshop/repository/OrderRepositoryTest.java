522353e60a616a0794635ba3f9e96440b4e0b5e5
alif <malifalhakim11@gmail.com>
2024-03-02 22:24:09 +0700

    Fix: Add product3 to products in OrderRepositoryTest

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
@@ -35,6 +35,7 @@ public class OrderRepositoryTest {
         orders.add(order2);
         Order order3 = new Order("e334ef40-9eff-4da8-9487-8ee697ecbf1e",
                 products, 1708570000L,"Bambang Sudrajat");
+        orders.add(order3);
     }
 
     @Test
